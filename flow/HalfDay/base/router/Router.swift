04672f108101ded81fb5d3c0e230232943e335d4
wangyongyue <wangyouyue@kingbirdplus.com>
2019-03-11 09:48:42 +0800

    router

---
+++ flow/HalfDay/base/router/Router.swift
@@ -0,0 +1,13 @@
+//
+//  Router.swift
+//  HalfDay
+//
+//  Created by apple on 2019/3/11.
+//  Copyright © 2019 wangyongyue. All rights reserved.
+//
+
+import UIKit
+
+class Router: NSObject {
+
+}
